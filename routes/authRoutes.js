63ece731e407c32453af44ac1d7f1e9fd77f177a
Bhavya Khatri <115460646+Bhavya022@users.noreply.github.com>
2025-01-12 13:52:39 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
-router.post('/logout', authenticateUser, logout);
+router.post('/logout', logout);
 
 module.exports = router;
