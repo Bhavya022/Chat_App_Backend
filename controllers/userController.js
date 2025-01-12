e09edc09cdbc68cbe906fdf5f123cfba23c19eb1
Bhavya Khatri <115460646+Bhavya022@users.noreply.github.com>
2025-01-12 15:14:30 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -35,6 +35,6 @@ const activeuser = async (req, res) => {
     } catch (err) {
         res.status(500).json({ message: 'Error fetching active users' });
     }
-});
+};
 
 module.exports = { getUserStatus, updateUserStatus,activeuser };
