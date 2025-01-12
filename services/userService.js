00baddace0e2785b79ac6f3f6fda0d0c54e29fa9
Bhavya Khatri <115460646+Bhavya022@users.noreply.github.com>
2025-01-12 14:02:36 +0530

    Update userService.js

--- services/userService.js
+++ services/userService.js
@@ -1,13 +1,20 @@
 const User = require('../models/User');
 
 const updateUserStatus = async (userId, onlineStatus) => {
-  try { 
-    console.log(userId, onlineStatus)
-    await User.findByIdAndUpdate(userId, { online: onlineStatus });
+  try {
+    console.log(userId, onlineStatus);
+    const updatedUser = await User.findByIdAndUpdate(userId, { online: onlineStatus }, { new: true });
+    if (!updatedUser) {
+      throw new Error('User not found');
+    }
+    console.log('User status updated:', updatedUser);
+
   } catch (err) {
+    
     throw new Error('Error updating user status: ' + err.message);
   }
 };
+
 
 const getUserById = async (userId) => {
   try {
