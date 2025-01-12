0fe7e9144f2a0d64242e539d237c6e3681358f54
Bhavya Khatri <115460646+Bhavya022@users.noreply.github.com>
2025-01-12 14:17:54 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,9 +5,9 @@ const getUserStatus = async (req, res) => {
   const { userId } = req.params;
   try {
     const user = await userService.getUserById(userId);
-    res.status(200).json({ status: user.online });
+    return res.status(200).json({ status: user.online });
   } catch (err) {
-    res.status(500).json({ message: 'Error fetching user status' });
+    return res.status(500).json({ message: 'Error fetching user status' });
   }
 };
 
@@ -15,12 +15,12 @@ const updateUserStatus = async (req, res) => {
   const { userId, onlineStatus } = req.body;
   try {
     if(!userId || !onlineStatus) {
-      res.status(400).json({ message: 'User ID and online status are required' });
+      return res.status(400).json({ message: 'User ID and online status are required' });
     }
     await userService.updateUserStatus(userId, onlineStatus);
-    res.status(200).json({ message: 'User status updated' });
+    return res.status(200).json({ message: 'User status updated' });
   } catch (err) {
-    res.status(500).json({ message: 'Error updating user status controller' });
+   return res.status(500).json({ message: 'Error updating user status controller' });
   }
 };
 
